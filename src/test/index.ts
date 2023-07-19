6df35c8ce3e2fc2d7799dfd0ce5c92f811c0ed81
Tom Daniel <0xtomdaniel@gmail.com>
2023-07-18 18:02:33 -0600

    Additional setup

---
+++ src/test/index.ts
@@ -0,0 +1,9 @@
+import "chai/register-should";
+
+describe("Register new user", () => {
+  describe("Scenario 1: Successfully register new user", () => {
+    it("", () => {
+      // ...
+    });
+  });
+});
