e8c32af683d39149efaff6015625f8a87282f723
Tom Daniel <0xtomdaniel@gmail.com>
2023-07-25 16:47:09 -0600

    Fixed error handling & other clean up

--- demo/web3account/src/main.ts
+++ demo/web3account/src/main.ts
@@ -5,7 +5,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>Web3Accounts</h1>
     <h2>by Firepot</h2>
-    <div">
+    <div>
       <label for="username">Username:</label>
       <input type="text" id="username" name="username" required>
       <br>
@@ -16,33 +16,25 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   </div>
 `;
 
-// <button>
 const elemBegin = document.getElementById("register");
-// <span>/<p>/etc...
 const elemSuccess = document.getElementById("success");
-// <span>/<p>/etc...
 const elemError = document.getElementById("error");
 const elemUsername = document.querySelectorAll("input")[0];
 
 elemBegin?.addEventListener("click", async () => {
-  // Reset success/error messages
   elemSuccess!.innerHTML = "";
   elemError!.innerHTML = "";
 
   try {
-    // Pass the options to the authenticator and wait for a response
     await registerUser(elemUsername.value);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
-    // Some basic error handling
     if (error.name === "InvalidStateError") {
       elemError!.innerText =
         "Error: Authenticator was probably already registered by user";
     } else {
       elemError!.innerText = error;
     }
-
-    throw error;
   }
 
   elemSuccess!.innerHTML = "Success!";
